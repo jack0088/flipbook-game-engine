8903e11566938324e283d6dbef73c453fd4a8d3d
aschaefer <aschaefer@aschaefers-iMac.local>
2019-05-08 15:36:21 +0200

    made preloader and cache methods work but need to implement draw method and dynamic preloading; also need to specify how triggers precedence is handled

--- main.lua
+++ main.lua
@@ -24,53 +24,68 @@ end
 local json = require "json"
 local Chapter = class()
 
-function Chapter:open(script)
+function Chapter:open(script, buffer)
     self.setup = json.decode(love.filesystem.read(script))
-    self.asset = {}
-    self.frame = self.setup.setting.frame
+    self.buffer = buffer or 30 -- length in seconds
     love.window.setTitle(self.setup.camera.title)
     love.window.setMode(self.setup.camera.width, self.setup.camera.height, {
         fullscreen = self.setup.camera.fullscreen,
         resizable = true
     })
-    self:preload(30)
-    -- play visual transition effect
-    for _, a in ipairs(self.asset) do
+    for _, a in ipairs(self.playlist) do
         for b, c in pairs(a) do
             print(_, b, c)
         end
     end
+    -- TODO play visual transition effect
     return self
 end
 
 function Chapter:close()
-    -- play visual transition effect
-    -- unload/ destroy things if needed
+    -- TODO play visual transition effect
+    -- unload/ destroy current chapter if needed
 end
 
 function Chapter:preload(seconds)
-    local remainder = seconds
-    for index, frame in ipairs(self.setup.scene) do
+    local remainder, index = seconds
+    if self.playlist and self.playhead then index = self.playlist[self.playhead].index
+    else index = self.setup.setting.frame end
+    self.playlist = {}
+    self.playhead = 1
+    repeat
+        self:cache(index)
+        local earliest
+        for k, trigger in ipairs(self.setup.scene[index].trigger) do
+            local delay = (self.playlist[#self.playlist].audio and self.playlist[#self.playlist].audio:getDuration() or trigger.delay) or 0
+            if not earliest or earliest > delay then
+                earliest = delay
+                index = trigger.frame
+            end
+        end
+        remainder = remainder - earliest
+    until remainder <= 0 or not index
+end
+
+function Chapter:cache(frame)
+    if type(frame) == "number" then
         local context = love.graphics.newCanvas(self.setup.setting.width, self.setup.setting.height)
         love.graphics.setCanvas(context)
-        for i, layer in ipairs(frame.layer) do
+        for i, layer in ipairs(self.setup.scene[frame].layer) do
             love.graphics.draw(love.graphics.newImage(self.setup.setting.folder..layer))
         end
         love.graphics.setCanvas()
-        self.asset[index] = {render = context:newImageData()}
-        for j, trigger in ipairs(frame.trigger) do
+        table.insert(self.playlist, {render = context:newImageData(), index = frame})
+        for j, trigger in ipairs(self.setup.scene[frame].trigger) do
             if type(trigger.audio) == "string" then
-                assert(not self.asset[index].audio, "found audio trigger duplicate in scene frame "..index)
-                self.asset[index].audio = love.audio.newSource(self.setup.setting.folder..trigger.audio, "static")
+                assert(not self.playlist[#self.playlist].audio, "found audio trigger duplicate in scene frame "..frame)
+                self.playlist[#self.playlist].audio = love.audio.newSource(self.setup.setting.folder..trigger.audio, "static")
             end
-            local delay = (self.asset[index].audio and self.asset[index].audio:getDuration() or trigger.delay) or 0
-            remainder = remainder - delay
         end
-        if remainder <= 0 then break end
     end
 end
 
 function Chapter:draw()
+    self:preload(self.buffer) -- TODO dynamically
     love.graphics.setBackgroundColor(self.setup.camera.chroma)
 end
 
