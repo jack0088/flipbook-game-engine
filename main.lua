b09ed7e374d5a46847c405f2835f7b5e65209cbd
aschaefer <aschaefer@aschaefers-iMac.local>
2019-05-08 11:27:08 +0200

    refactored code but need to implement self.frame inside the preload method because otherwise the loop start is wrong, because alsways at the beginnning of table

--- main.lua
+++ main.lua
@@ -24,45 +24,50 @@ end
 local json = require "json"
 local Chapter = class()
 
-function Chapter:load(script)
+function Chapter:open(script)
     self.setup = json.decode(love.filesystem.read(script))
     self.asset = {}
     self.frame = self.setup.setting.frame
-
-    for i, frame in ipairs(self.setup.scene) do
-        local context = love.graphics.newCanvas(self.setup.setting.width, self.setup.setting.height)
-        love.graphics.setCanvas(context)
-        for j, layer in ipairs(frame.layer) do
-            love.graphics.draw(love.graphics.newImage(self.setup.setting.folder..layer))
-        end
-        love.graphics.setCanvas()
-        self.asset[i] = {render = context:newImageData()}
-        for k, trigger in ipairs(frame.trigger) do
-            if type(trigger.audio) == "string" then
-                assert(not self.asset[i].audio, "found audio trigger duplicate in scene frame "..i)
-                self.asset[i].audio = love.audio.newSource(self.setup.setting.folder..trigger.audio, "static")
-            end
-        end
-    end
-
     love.window.setTitle(self.setup.camera.title)
     love.window.setMode(self.setup.camera.width, self.setup.camera.height, {
         fullscreen = self.setup.camera.fullscreen,
         resizable = true
     })
-
+    self:preload(30)
+    -- play visual transition effect
+    for _, a in ipairs(self.asset) do
+        for b, c in pairs(a) do
+            print(_, b, c)
+        end
+    end
     return self
 end
 
-function Chapter:preload(length)
-end
-
-function Chapter:open()
-    -- visual transition effect
-end
-
 function Chapter:close()
-    -- visual transition effect
+    -- play visual transition effect
+    -- unload/ destroy things if needed
+end
+
+function Chapter:preload(seconds)
+    local remainder = seconds
+    for index, frame in ipairs(self.setup.scene) do
+        local context = love.graphics.newCanvas(self.setup.setting.width, self.setup.setting.height)
+        love.graphics.setCanvas(context)
+        for i, layer in ipairs(frame.layer) do
+            love.graphics.draw(love.graphics.newImage(self.setup.setting.folder..layer))
+        end
+        love.graphics.setCanvas()
+        self.asset[index] = {render = context:newImageData()}
+        for j, trigger in ipairs(frame.trigger) do
+            if type(trigger.audio) == "string" then
+                assert(not self.asset[index].audio, "found audio trigger duplicate in scene frame "..index)
+                self.asset[index].audio = love.audio.newSource(self.setup.setting.folder..trigger.audio, "static")
+            end
+            local delay = (self.asset[index].audio and self.asset[index].audio:getDuration() or trigger.delay) or 0
+            remainder = remainder - delay
+        end
+        if remainder <= 0 then break end
+    end
 end
 
 function Chapter:draw()
@@ -73,7 +78,7 @@ end
 
 
 function love.load()
-    bouncing_ball = Chapter():load("demo/bouncing-ball.json")
+    bouncing_ball = Chapter():open("demo/bouncing-ball.json")
     --for k, v in pairs(bouncing_ball) do print(k, v) end
 end
 
