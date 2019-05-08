aa91388e32931919a67f5924389d22b3cdcf2b11
jack0088 <jack0088@me.com>
2019-05-08 22:46:54 +0200

    should work, but I can not see anything and also strange error at certain frame happens later in time

--- main.lua
+++ main.lua
@@ -26,9 +26,7 @@ local Chapter = class()
 
 function Chapter:open(script)
     self.setup = json.decode(love.filesystem.read(script))
-    self.renderer = {}
-    self.timer = {}
-    self:preload()
+    self:preload(self.setup.setting.frame)
     love.window.setTitle(self.setup.camera.title)
     love.window.setMode(self.setup.camera.width, self.setup.camera.height, {
         fullscreen = self.setup.camera.fullscreen,
@@ -43,61 +41,82 @@ function Chapter:close()
     -- unload/ destroy current chapter if needed
 end
 
-function Chapter:preload()
-    local index = self.setup.setting.frame repeat
-        for t, trigger in ipairs(self.setup.scene[index].trigger) do
-            if trigger.frame then
-                local INSTANT = not trigger.delay and not trigger.audio and not trigger.contact
-                local FORWARD = trigger.delay or trigger.audio
-                local SLEEP = not trigger.delay and not trigger.audio and trigger.contact
-                
-                
+function Chapter:preload(frame)
+    -- queue up all trigger callbacks
+    self.setup.setting.frame = frame
+    self.trigger = {}
+    for t, trigger in ipairs(self.setup.scene[self.setup.setting.frame].trigger) do
+        if trigger.frame then
+            local SKIP = not trigger.delay and not trigger.audio and not trigger.contact
+            local FORWARD = trigger.delay or trigger.audio
+            local DELAY = trigger.delay and trigger.audio
+            local SLEEP = not trigger.delay and not trigger.audio and trigger.contact
+            local SAVE = trigger.autosave
+            local audio = trigger.audio and love.audio.newSource(self.setup.setting.folder..trigger.audio, "static") or nil
 
-                self:queue(self.timer, nil)
-                index = trigger.frame
-                if SLEEP then break end
-                if FORWARD then remainder = remainder - earliest end
+            -- play audio after delay or immediately
+            if audio then
+                local timeout = love.timer.getTime() + (trigger.delay or 0)
+                table.insert(self.trigger, function()
+                    if love.timer.getTime > timeout and not audio:isPlaying() then
+                        audio:play()
+                    end
+                end)
+            end
+            
+            -- forward immediately
+            if SKIP then
+                table.insert(self.trigger, function()
+                    self:preload(trigger.frame)
+                end)
+            end
+
+            -- forward after delay, audio or both
+            if FORWARD then
+                local timeout = love.timer.getTime()
+                if DELAY then timeout = timeout + trigger.delay + audio:getDuration()
+                elseif audio then timeout = timeout + audio:getDuration()
+                else timeout = timeout + trigger.delay end
+                table.insert(self.trigger, function()
+                    if love.timer.getTime() > timeout then
+                        self:preload(trigger.frame)
+                    end
+                end)
+            end
+
+            -- forward after click or touch
+            if SLEEP then
+                local aabb = trigger.contact
+                table.insert(self.trigger, function()
+                    if love.mouse.isDown(1)
+                    and love.mouse.getX() > aabb[1] and love.mouse.getX() < aabb[3]
+                    and love.mouse.getY() > aabb[2] and love.mouse.getY() < aabb[4]
+                    then
+                        self:preload(trigger.frame)
+                    end
+                end)
+            end
+
+            -- store game progress
+            if SAVE then
+                -- TODO
             end
         end
-
-        local context = love.graphics.newCanvas(self.setup.setting.width, self.setup.setting.height)
-        love.graphics.setCanvas(context)
-        for l, layer in ipairs(self.setup.scene[index].layer) do
-            love.graphics.draw(love.graphics.newImage(self.setup.setting.folder..layer))
-        end
-        love.graphics.setCanvas()
-        self:queue(self.renderer, love.graphics.drawFrame, self, context:newImageData(), duration)
-    until not index
-
-    for _, a in ipairs(self.playlist) do
-        for b, c in pairs(a) do
-            print(_, b, c)
-        end
     end
-end
 
-function Chapter:queue(thread, action, ...)
-    local arguments = {...}
-    local process = function(reference) action(reference, unpack(arguments)) end
-    table.insert(thread, coroutine.create(process))
-end
-
-function Chapter:drawFrame(image, duration)
-    while love.timer.getTime() <= love.timer.getTime() + duration do
-        love.graphics.draw(image)
-        if type(self) == "thread" then coroutine.yield() end
+    -- bake all layer images into a single texture
+    self.render = love.graphics.newCanvas(self.setup.setting.width, self.setup.setting.height)
+    love.graphics.setCanvas(self.render)
+    for l, layer in ipairs(self.setup.scene[self.setup.setting.frame].layer) do
+        love.graphics.draw(love.graphics.newImage(self.setup.setting.folder..layer))
     end
+    love.graphics.setCanvas()
 end
 
 function Chapter:draw()
-    --[[
-    --self:preload(self.buffer) -- TODO dynamically
     love.graphics.setBackgroundColor(self.setup.camera.chroma)
-    if #self.pipeline > 0 then
-        if coroutine.status(self.pipeline[1]) == "dead" then table.remove(self.pipeline, 1)
-        else coroutine.resume(self.pipeline[1], self.pipeline[1]) end
-    end
-    --]]
+    love.graphics.draw(self.render)
+    for t, trigger in ipairs(self.trigger) do trigger() end
 end
 
 
